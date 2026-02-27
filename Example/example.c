79fe1829adc250aa7a7e5daa654e9c39938725ad
Eden Sheiko <77243045+Eden-Sheiko@users.noreply.github.com>
2026-02-27 18:56:11 +0200

    rmv tests *testing

--- Example/example.c
+++ Example/example.c
@@ -2,7 +2,7 @@
 #include "../Inc/i2c_log.h"
 
 int main(void){
-    printf("start program testing \n");
+    LOG_INFO("--- I2C Example App ---");
     uint8_t data = 0xff;
     i2c_module_config_t cfg = {
         .addr =  0x10,
@@ -15,8 +15,21 @@ int main(void){
         LOG_ERROR("module didnt load up\n");
     }
     if (i2c_device_write(i2c, &data, 1) != I2C_OK) {
-        printf("we have an error\n");
+        LOG_ERROR("write failed\n");
     }
+    if (i2c_device_set_addr(0x55, i2c) != I2C_OK) {
+        LOG_ERROR("Set addr failed\n");
+    }
+    if (i2c_device_write(i2c, &data, 1) != I2C_OK) {
+        LOG_ERROR("write failed\n");
+    }
+    i2c_device_toggle_logger(false ,i2c);
+    i2c_device_toggle_logger(true ,i2c);
+
+    if (i2c_device_reg_write(i2c ,0x12 ,&data, 1) != I2C_OK) {
+        LOG_ERROR("write failed\n");
+    }
+
 
     i2c_device_destroy(i2c);
     return 0;
